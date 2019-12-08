dc0064412138aab9a4f152b1967d933bb75cf7af
root <root@localhost.localdomain>
2019-12-08 17:11:05 +0530

    Serve

---
+++ client/build/precache-manifest.ae35ccf5c75b1a373f5b9afbcaf6ebb4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29a77c19f3cd441c532a12495925fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be95421aa6c825e436d3",
+    "url": "/static/css/main.4db74f1e.chunk.css"
+  },
+  {
+    "revision": "14ae6a53ae5309a10013",
+    "url": "/static/js/2.ad65fe04.chunk.js"
+  },
+  {
+    "revision": "be95421aa6c825e436d3",
+    "url": "/static/js/main.13c2f58c.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "fcfbe1fd1df0d4e01b15cff569bab3f5",
+    "url": "/static/media/Polygon.fcfbe1fd.svg"
+  },
+  {
+    "revision": "d85992f7cf5ea454de332053bb2f3084",
+    "url": "/static/media/header.d85992f7.png"
+  },
+  {
+    "revision": "fc9f42dfe9050cf1f7be8132315617e0",
+    "url": "/static/media/img1.fc9f42df.png"
+  },
+  {
+    "revision": "ba1d96565bce09160162068ff24baa91",
+    "url": "/static/media/plate.ba1d9656.svg"
+  },
+  {
+    "revision": "226b00a48bfb3d2034d7163135a1153f",
+    "url": "/static/media/plato.226b00a4.svg"
+  }
+]);
