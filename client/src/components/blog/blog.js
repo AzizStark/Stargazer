319dc42d5f7dd2ef602f8ea074335be1b20b44b2
AzizStark <rrahman182@gmail.com>
2020-02-21 00:20:33 +0530

    Define meta

--- client/src/components/blog/blog.js
+++ client/src/components/blog/blog.js
@@ -96,6 +96,7 @@ shouldComponentUpdate(nextProps, nextState) {
       <div className={bstyles.blog} style={{overflow: 'Hidden'}}>
          <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&display=swap" rel="stylesheet"/>
          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+         <meta name="description" content="Explore more on Aziz Stark's blog"/>
         <Navba></Navba>
         <div style={{height: '100%'}}>
           <h1 className={bstyles.sidebar}>AZIZSTARK'S BLOG</h1>
