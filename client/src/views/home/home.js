d35b078cf0c7199119da920a9e2e185530ed4262
AzizStark <theazizstark@gmail.com>
2020-03-12 22:25:57 +0530

    Add analytics

--- client/src/views/home/home.js
+++ client/src/views/home/home.js
@@ -87,6 +87,8 @@ class Home extends Component {
         <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,500,600,700|Nunito:200,300,400,400i,600,700&display=swap" rel="stylesheet"></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta name="description" content="Hello! I am Aziz Stark, An aspiring developer and designer." />
+	<script async defer src="https://cdn.simpleanalytics.io/hello.js"></script>
+	<noscript><img src="https://api.simpleanalytics.io/hello.gif" alt=""/></noscript>
         <section className={`hero is-fullheight ${cstyles.svgg}`}>
           <Navba></Navba>
           <div className="hero-body" >
